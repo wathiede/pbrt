e855318fa836b31e007c7cdc2683330094d579d9
Bill Thiede <git@xinu.tv>
2018-01-18 06:42:00 -0800

    Use parse_scene to actually parse scene file.

--- src/core/api.rs
+++ src/core/api.rs
@@ -3,7 +3,11 @@ use std::io::Read;
 use std::io;
 use std::path::Path;
 
+extern crate nom;
+use self::nom::IResult;
+
 use core::pbrt::Float;
+use core::parser;
 
 // Pbrt is the top-level global container for all rendering functionality.
 #[derive(Debug)]
@@ -14,10 +18,17 @@ impl Pbrt {
         Pbrt {}
     }
 
-    pub fn parse_file<P: AsRef<Path>>(&self, path: P) -> Result<String, io::Error> {
+    pub fn parse_file<P: AsRef<Path>>(&self, path: P) -> Result<parser::Scene, io::Error> {
         let mut f = File::open(path)?;
-        let mut s = String::new();
-        f.read_to_string(&mut s).map(|_| s)
+        let mut buffer = Vec::new();
+
+        // read the whole file
+        f.read_to_end(&mut buffer)?;
+        match parser::parse_scene(&buffer[..]) {
+            IResult::Done(_, res) => Ok(res),
+            IResult::Error(ref e) => panic!("e: {:#?}", e),
+            IResult::Incomplete(n) => panic!("need: {:?}", n),
+        }
     }
 
     pub fn look_at(
