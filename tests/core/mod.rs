4f4444bffb224b51bfb0b931197e78b1d70e9f14
Bill Thiede <git@xinu.tv>
2019-01-04 15:11:54 -0800

    Add severe! error macro, and multi-directory integration tests.

--- tests/core/mod.rs
+++ tests/core/mod.rs
@@ -1,4 +1,4 @@
-// Copyright 2018 Google LLC
+// Copyright 2019 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,16 +11,4 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-pub mod api;
-pub mod error;
-pub mod geometry;
-pub mod interaction;
-pub mod light;
-pub mod medium;
-pub mod paramset;
-pub mod pbrt;
-pub mod spectrum;
-pub mod texture;
-pub mod transform;
-
-mod parser;
+mod error;
--- tests/integration.rs
+++ tests/integration.rs
@@ -1,4 +1,4 @@
-// Copyright 2018 Google LLC
+// Copyright 2019 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,16 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-pub mod api;
-pub mod error;
-pub mod geometry;
-pub mod interaction;
-pub mod light;
-pub mod medium;
-pub mod paramset;
-pub mod pbrt;
-pub mod spectrum;
-pub mod texture;
-pub mod transform;
+#[macro_use]
+extern crate pbrt;
+#[macro_use]
+extern crate log;
 
-mod parser;
+mod core;
