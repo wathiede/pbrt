9d995ff43452e7030a6cf8f2e13f5c4a3b29bf1a
Bill Thiede <git@xinu.tv>
2019-12-03 18:42:08 -0800

    film: implement stub FilmTile.

--- src/core/film.rs
+++ src/core/film.rs
@@ -28,6 +28,11 @@ use crate::Float;
 
 const FILTER_TABLE_WIDTH: usize = 16;
 
+struct FilmTilePixel {
+    contrib_sum: Spectrum,
+    filter_weight_sum: Float,
+}
+
 struct Pixel {
     xyz: [Float; 3],
     filter_weight_sum: Float,
@@ -35,6 +40,8 @@ struct Pixel {
     _pad: Float,
 }
 
+/// Film models the sensor on a simulated camera.  It may have a `crop_window` that limits
+/// rendering to a subset of the `Film`.
 pub struct Film {
     pub full_resolution: Point2i,
     crop_window: Bounds2f,
@@ -114,7 +121,7 @@ impl Film {
     /// let filter = BoxFilter::new([8., 8.].into());
     /// let film = Film::new(
     ///     [1920, 1080].into(),
-    ///     ([0.25, 0.25], [0.75, 0.75]).into(),
+    ///     [[0.25, 0.25], [0.75, 0.75]].into(),
     ///     Box::new(filter),
     ///     35.0,
     ///     "output.png".to_string(),
@@ -123,18 +130,17 @@ impl Film {
     /// );
     /// assert_eq!(
     ///     film.get_sample_bounds(),
-    ///     Bounds2i::from(([472, 262], [1448, 818]))
+    ///     Bounds2i::from([[472, 262], [1448, 818]])
     /// );
     /// ```
     pub fn get_sample_bounds(&self) -> Bounds2i {
-        Bounds2f::from((
-            (Point2f::from(self.cropped_pixel_bounds.p_min) + Vector2f::from([0.5, 0.5])
-                - self.filter.radius())
-            .floor(),
-            (Point2f::from(self.cropped_pixel_bounds.p_max) - Vector2f::from([0.5, 0.5])
-                + self.filter.radius())
-            .ceil(),
-        ))
+        let half_pixel = Vector2f::from([0.5, 0.5]);
+        Bounds2f::from([
+            (Point2f::from(self.cropped_pixel_bounds.p_min) + half_pixel - self.filter.radius())
+                .floor(),
+            (Point2f::from(self.cropped_pixel_bounds.p_max) - half_pixel + self.filter.radius())
+                .ceil(),
+        ])
         .into()
     }
 
@@ -150,7 +156,7 @@ impl Film {
     /// let diag_mm = 100.;
     /// let film = Film::new(
     ///     [800, 600].into(),
-    ///     ([0., 0.], [1., 1.]).into(),
+    ///     [[0., 0.], [1., 1.]].into(),
     ///     Box::new(filter),
     ///     diag_mm,
     ///     "output.png".to_string(),
@@ -159,13 +165,14 @@ impl Film {
     /// );
     /// assert_eq!(
     ///     film.get_physical_extent(),
-    ///     Bounds2f::from(([-0.04, -0.03], [0.04, 0.03]))
+    ///     Bounds2f::from([[-0.04, -0.03], [0.04, 0.03]])
     /// );
     ///
     /// let filter = BoxFilter::new([8., 8.].into());
     /// let film = Film::new(
     ///     [800, 600].into(),
-    ///     ([0.25, 0.25], [0.75, 0.75]).into(),
+    ///     // The result of get_physical_extent doesn't change if crop_window is a subset of the Film.
+    ///     [[0.25, 0.25], [0.75, 0.75]].into(),
     ///     Box::new(filter),
     ///     diag_mm,
     ///     "output.png".to_string(),
@@ -174,22 +181,76 @@ impl Film {
     /// );
     /// assert_eq!(
     ///     film.get_physical_extent(),
-    ///     Bounds2f::from(([-0.04, -0.03], [0.04, 0.03]))
+    ///     Bounds2f::from([[-0.04, -0.03], [0.04, 0.03]])
     /// );
     /// ```
     pub fn get_physical_extent(&self) -> Bounds2f {
         let aspect = self.full_resolution.y as Float / self.full_resolution.x as Float;
         let x = (self.diagonal_m * self.diagonal_m / (1. + aspect * aspect)).sqrt();
         let y = aspect * x;
-        (
+        [
             Point2f::from([-x / 2., -y / 2.]),
             Point2f::from([x / 2., y / 2.]),
-        )
-            .into()
+        ]
+        .into()
     }
 
-    pub fn get_film_tile(&self, sample_bounds: &Bounds2i) -> FilmTile {
-        unimplemented!()
+    /// Create a `FilmTile` representing the subregion of this `Film` denoted by `sample_bounds`.
+    /// The `FilmTile` should have its pixels contributed to the `Film` by calling
+    /// `merge_film_tile`.
+    ///
+    /// # Examples
+    /// ```
+    /// use pbrt::core::film::Film;
+    /// use pbrt::core::geometry::Bounds2i;
+    /// use pbrt::filters::boxfilter::BoxFilter;
+    ///
+    /// let filter = BoxFilter::new([8., 8.].into());
+    /// let film = Film::new(
+    ///     [1920, 1080].into(),
+    ///     [[0.25, 0.25], [0.75, 0.75]].into(),
+    ///     Box::new(filter),
+    ///     35.0,
+    ///     "output.png".to_string(),
+    ///     1.,
+    ///     1.,
+    /// );
+    ///
+    /// // Tile bigger than Film's crop area gets clipped to Film's crop area.
+    /// assert_eq!(
+    ///     film.get_film_tile(Bounds2i::from([[0, 0], [1920, 1080]]))
+    ///         .get_pixel_bounds(),
+    ///     Bounds2i::from([[1920 / 4, 1080 / 4], [3 * 1920 / 4, 3 * 1080 / 4]])
+    /// );
+    /// // Tile smaller than Film's crop area is the given bound expanded by half the filter size.
+    /// assert_eq!(
+    ///     film.get_film_tile(Bounds2i::from([[500, 500], [600, 600]]))
+    ///         .get_pixel_bounds(),
+    ///     Bounds2i::from([[492, 492], [608, 608]])
+    /// );
+    /// ```
+    pub fn get_film_tile(&self, sample_bounds: Bounds2i) -> FilmTile {
+        let half_pixel = Vector2f::from([0.5, 0.5]);
+        let float_bounds = Bounds2f::from(sample_bounds);
+        let p0 = Point2i::from((float_bounds.p_min - half_pixel - self.filter.radius()).ceil());
+        let p1 = Point2i::from(
+            (float_bounds.p_max - half_pixel + self.filter.radius()).floor()
+                + Point2f::from([1., 1.]),
+        );
+        let tile_pixel_bounds =
+            Bounds2i::intersect(&Bounds2i::from([p0, p1]), &self.cropped_pixel_bounds);
+        FilmTile::new(
+            tile_pixel_bounds,
+            self.filter.radius(),
+            &self.filter_table,
+            FILTER_TABLE_WIDTH,
+            self.max_sample_luminance,
+        )
+        /*
+        return std::unique_ptr<FilmTile>(new FilmTile(
+        tilePixelBounds, filter->radius, filterTable, filterTableWidth,
+        maxSampleLuminance));
+        */
     }
 
     pub fn merge_film_tile(&self, tile: FilmTile) {
@@ -213,4 +274,45 @@ impl Film {
     }
 }
 
-pub struct FilmTile {}
+/// FilmTile represents a subarea of `Film` within the `Film`'s configured sampling bounds.  It
+/// allows rendering of portions of the `Film` to be handed off to separate threads, and the final
+/// assembly of the full image is handled by passing the `FilmTile` back to the `Film` via
+/// [merge_film_tile]
+///
+/// [merge_film_tile]: Film::merge_film_tile
+pub struct FilmTile<'ft> {
+    pixel_bounds: Bounds2i,
+    filter_radius: Vector2f,
+    inv_filter_radius: Vector2f,
+    filter_table: &'ft Vec<Float>,
+    filter_table_size: usize,
+    max_sample_luminance: Float,
+    pixels: Vec<FilmTilePixel>,
+}
+
+impl<'ft> FilmTile<'ft> {
+    fn new(
+        pixel_bounds: Bounds2i,
+        filter_radius: Vector2f,
+        filter_table: &'ft Vec<Float>,
+        filter_table_size: usize,
+        max_sample_luminance: Float,
+    ) -> FilmTile<'ft> {
+        FilmTile {
+            pixel_bounds,
+            filter_radius,
+            inv_filter_radius: [1. / filter_radius.x, 1. / filter_radius.y].into(),
+            filter_table,
+            filter_table_size,
+            pixels: Vec::new(),
+            max_sample_luminance,
+        }
+    }
+
+    /// Get the pixel bounds for this tile.  For example see [get_film_tile].
+    ///
+    /// [get_film_tile]: Film::get_film_tile
+    pub fn get_pixel_bounds(&self) -> Bounds2i {
+        self.pixel_bounds
+    }
+}
